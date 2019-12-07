00ff0409cf2875168eb2cb0b6b943555bc80e547
Rodrvc <silver.rvc@gmail.com>
2019-12-07 12:58:55 -0300

    primer push

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php 
+
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'proyecto_gamer'
+);
+
+if(isset($conn)){
+    echo 'db is conected' ;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Paginas de video juegos</title>
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" sizes="192x192" href="img/fav/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/fav/favicon-32x32.png">
+</head>
+
+<body>
+    <header>
+        <img src="img/logo.png" width="100" height="100">
+        <h1 class="titulo">GAMER NEW CHECK </h1>
+        <ul>
+            <li class="linav"><a href="index.html">Inicio</a></li>
+            <li class="linav"><a href="trucos.html">Trucos</a></li>
+            <li class="linav"><a href="Nintendo.html">Nintendo</a></li>
+        </ul>
+    </header>
+    <section class="main-container">
+        <div class="section">
+            <div class="container container-icon">
+                <h1 class="main-pag-title ">Noticias del mundo Gamer</h1>
+                <p class="principal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam exercitationem
+                    dolore quibusdam,
+                    id sapiente saepe autem at quae adipisci tempora maiores inventore voluptatibus laborum suscipit
+                    rem. Nobis, ullam! Necessitatibus, sint!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam exercitationem dolore quibusdam,
+                    id sapiente saepe autem at quae adipisci tempora maiores inventore voluptatibus laborum suscipit
+                    rem. Nobis, ullam! Necessitatibus, sint!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam exercitationem dolore quibusdam,
+                    id sapiente saepe autem at quae adipisci tempora maiores inventore voluptatibus laborum suscipit
+                    rem. Nobis, ullam! Necessitatibus, sint!</p>
+                <h2>Tus mejores juegos Semanales!</h2>
+                <img class="icons i1 joystick" src="img/game-console.svg" alt="">
+                <img class="icons i3 joystick" src="img/game-controller.png" alt="">
+                <img class="icons i2 joystick" src="img/gamepad.png" alt="">
+            </div>
+            <div class="container">
+                <h2>Esto es una post </h2>
+                <p class="article">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repellat sequi, nisi
+                    ipsum
+                    perferendis iusto?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nobis aperiam pariatur dolore
+                    voluptatibus iusto consequatur quam tempora labore expedita officia placeat ut, assumenda, est
+                    incidunt quo consequuntur, architecto ipsam!
+
+                </p>
+            </div>
+
+            <hr align="center">
+            <div class="container">
+                <h2>Esto es una post </h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repellat sequi, nisi ipsum
+                    perferendis iusto?
+                    Delectus, fugiat maxime. Cum quae adipisci non quia dignissimos excepturi, sed ratione natus
+                    repudiandae earum.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum animi quo enim libero non
+                    perspiciatis unde dolor quia. Maxime obcaecati soluta rem ex a alias quis id ab quisquam temporibus!
+                </p>
+            </div>
+        </div>
+
+        <div class="aside">
+            <h4>Articulos Recomendados</h4>
+            <div class="container-aside">
+                <div class="card">
+                    <div class="body-card">
+                        <h5>Los 50 Juegos de la aaaaa Historia</h5>
+                        <div>
+                            <img src="img/videojuegos.jpg" class="imagen" alt="videojuegos">
+                        </div>
+                        <div>
+                            <p class="aside-text">Lorem ipsum dolor sit, </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="body-card">
+                        <h5>Los 50 Juegos de la aaaaa Historia</h5>
+                        <div>
+                            <img src="img/videojuegos.jpg" class="imagen" alt="videojuegos">
+                        </div>
+                        <div>
+                            <p class="aside-text">Lorem ipsum dolor sit, </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="body-card">
+                        <h5>Los 50 Juegos de la aaaaa Historia</h5>
+                        <div>
+                            <img src="img/videojuegos.jpg" class="imagen" alt="videojuegos">
+                        </div>
+                        <div>
+                            <p class="aside-text">Lorem ipsum dolor sit, </p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+    </section>
+    <footer>
+        <p> &copy;Alejantroll Rvc Systems</p>
+    </footer>
+</body>
+
+</html>
